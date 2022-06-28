e65ad4ce33151f0b999e91c599d82175dedf6309
Abbv75 <bore.younous59@gmail.com>
2022-06-28 17:49:38 +0200

    first commit

---
+++ java_objets/Medecin.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paradise.objets;
+
+/**
+ *
+ * @author Younouss boré
+ */
+public class Medecin {
+    public static String id="6";
+    public static String nom="";
+    public static String prenom="";
+    public static String email="";
+    public static String mot_de_passe="";
+    public static String role="";
+    public static String id_bloc="1";
+}
